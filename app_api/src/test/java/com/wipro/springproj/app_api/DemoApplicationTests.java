f1ffdbe05e887cf8e32222a40c372c14006d8926
Saurabh Raj <saurabhraj115@gmail.com>
2020-05-15 16:03:35 +0530

    first commit

---
+++ app_api/src/test/java/com/wipro/springproj/app_api/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.springproj.app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
