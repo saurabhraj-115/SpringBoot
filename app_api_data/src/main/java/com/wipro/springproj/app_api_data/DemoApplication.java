35463b4b2447bbc0bb6df2a09c3f4490e11886e9
Saurabh Raj <saurabhraj115@gmail.com>
2020-05-16 14:37:42 +0530

    .

---
+++ app_api_data/src/main/java/com/wipro/springproj/app_api_data/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.springproj.app_api_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
